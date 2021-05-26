59b563bbe1ff00d5d783fbde2b7905798c7ebdda
Kevin Johar <kevin.s.johar@gmail.com>
2021-05-26 19:41:35 -0400

    feat: game of life complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from 'src/app/projects/project/project.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
   ],
   providers: [],
+  exports: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
 import { GameOfLifeComponent } from './game-of-life/game-of-life.component';
 import { ProjectsRouting } from './projects.routing';
+import { CommonModule } from '@angular/common';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
     ProjectsPageComponent,
     GameOfLifeComponent,
+    ProjectComponent
   ],
   imports: [
-    ProjectsRouting
+    ProjectsRouting,
+    CommonModule,
   ]
 })
 export class ProjectsModule { }
